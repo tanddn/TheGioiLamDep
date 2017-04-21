12b70848d5e519d8302350c038441573efc3f597
tanddn <tanddn@gmail.com>
2017-04-21 16:37:47 +0700

    no message

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
-﻿$(document).ready(function () {
-    $('.navbar a.dropdown-toggle').on('click', function (e) {
-        var $el = $(this);
-        var $parent = $(this).offsetParent(".dropdown-menu");
-        $(this).parent("li").toggleClass('open');
+//$(document).ready(function () {
+//    $('.navbar a.dropdown-toggle').on('click', function (e) {
+//        var $el = $(this);
+//        var $parent = $(this).offsetParent(".dropdown-menu");
+//        $(this).parent("li").toggleClass('open');
 
-        if (!$parent.parent().hasClass('nav')) {
-            $el.next().css({ "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 });
-        }
+//        if (!$parent.parent().hasClass('nav')) {
+//            $el.next().css({ "top": $el[0].offsetTop, "left": $parent.outerWidth() - 4 });
+//        }
 
-        $('.nav li.open').not($(this).parents("li")).removeClass("open");
+//        $('.nav li.open').not($(this).parents("li")).removeClass("open");
 
-        return false;
-    });
-});
+//        return false;
+//    });
+//});
